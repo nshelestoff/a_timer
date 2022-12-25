766e3ad821b946dbef1c1ef6da02655b42f76881
Me <n.shelestoff@gmail.com>
2022-12-25 22:03:01 +0300

    changed bloc structure a bit

--- lib/src/timer_page.dart
+++ lib/src/timer_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:an_exercise_timer/src/timer.dart';
 import 'package:an_exercise_timer/src/actions.dart' as actions;
-import 'actions.dart';
 import 'background.dart';
 import 'timer_bloc/timer_bloc.dart';
 
@@ -32,8 +31,10 @@ class _TimerPageState extends State<TimerPage> {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (_) =>
-              TimerBloc(ticker: const Ticker(), sliderCubit: _sliderCubit, checkboxCubit: _checkboxCubit),
+          create: (_) => TimerBloc(
+              ticker: const Ticker(),
+              sliderCubit: _sliderCubit,
+              checkboxCubit: _checkboxCubit),
         ),
         BlocProvider(
           create: (_) => _checkboxCubit,
@@ -55,69 +56,78 @@ class TimerView extends StatefulWidget {
 }
 
 class _TimerViewState extends State<TimerView> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(title: const Center(
-            child: Text('Timer app'))),
-        body: Stack(
-          children: [
-            const Background(),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 100.0),
-                  child: Center(child: BlocBuilder<TimerBloc, TimerState>(
-                    builder: (context, state) {
-                      return TimerText();
-                    },
-                  )),
-                ),
-                const actions.Actions(),
-                BlocBuilder<CheckboxCubit, CheckboxState>(
+      key: _scaffoldKey,
+      appBar: AppBar(title: const Center(child: Text('Timer app'))),
+      body: Stack(
+        children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: BlocBuilder<TimerBloc, TimerState>(
                   builder: (context, state) {
-                    return Container(
-                      width: 180,
-                      child: CheckboxListTile(
-                        value: state.isChecked,
-                        title: Text('counting up', style: Theme.of(context).textTheme.subtitle1,),
-                        onChanged: (bool? value) {
-                          setState(() {
-                            context.read<CheckboxCubit>().changeValue(value!);
-                          });
-                        },
-                      ),
-                    );
+                    return const TimerText();
                   },
-                ),
-                Visibility(
-                  visible: !(context.read<CheckboxCubit>().state.isChecked),
-                  child: BlocBuilder<SliderCubit, SliderState>(
-                    builder: (context, state) {
-                      return Slider(
-                        value: state.value,
-                        max: 300,
-                        divisions: 10,
-                        label: state.value.round().toString(),
-                        onChanged: (double value) {
-                          setState(() {
-                            context.read<SliderCubit>().changeValue(value);
-                          });
-                        },
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      );
+                )),
+              ),
+              const actions.Actions(),
+              BlocBuilder<TimerBloc, TimerState>(
+                builder: (context, state) {
+                  return Visibility(
+                      visible: state is! TimerRunInProgress,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 180,
+                            child: CheckboxListTile(
+                              value: context.read<CheckboxCubit>().state.isChecked,
+                              title: Text(
+                                'counting up',
+                                style: Theme.of(context).textTheme.subtitle1,
+                              ),
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  context.read<CheckboxCubit>().changeValue(value!);
+                                });
+                              },
+                            ),
+                          ),
+                          Visibility(
+                            visible: !context.read<CheckboxCubit>().state.isChecked,
+                            child: Slider(
+                              value: context.read<SliderCubit>().state.value,
+                              max: 300,
+                              divisions: 10,
+                              label: context
+                                  .read<SliderCubit>()
+                                  .state
+                                  .value
+                                  .round()
+                                  .toString(),
+                              onChanged: (double value) {
+                                setState(() {
+                                  context.read<SliderCubit>().changeValue(value);
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ));
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
